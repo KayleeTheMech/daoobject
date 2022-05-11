e9e94855d96d83ecd8af8e46b062a178afcf90b8
KayleeTheMech <kaylee@posteo.de>
2022-05-11 17:11:46 +0200

    Update, delete, exists queries (no-issue)

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,14 @@ fn main() {
         twitter_account_id: 3,
         text: "test test test".to_string(),
     });
+    let exists = note_dao.exists(2, 3);
+
+    note_dao.update(TwitterNote {
+        user_id: 2,
+        twitter_account_id: 3,
+        text: "test2".to_string(),
+    });
     let note2 = note_dao.get(2, 3);
+    note_dao.delete(2, 3);
     println!("Tada")
 }
--- src/note.rs
+++ src/note.rs
@@ -12,8 +12,12 @@ pub struct TwitterNoteDAO {
 }
 const INSERT_NOTE_SQL: &str =
     "INSERT INTO notes (user_id, twitter_account_id, text) values (?1, ?2, ?3)";
-
 const SELECT_NOTE_SQL: &str = "SELECT * from notes WHERE user_id=?1 AND twitter_account_id=?2;";
+const DELETE_NOTE_SQL: &str = "DELETE from notes WHERE user_id=?1 AND twitter_account_id=?2;";
+const UPDATE_NOTE_SQL: &str =
+    "UPDATE notes SET text = ?3 WHERE user_id=?1 AND twitter_account_id=?2;";
+const EXISTS_NOTE_SQL: &str =
+    "SELECT EXISTS(SELECT * from notes WHERE user_id=?1 AND twitter_account_id=?2);";
 
 impl TwitterNoteDAO {
     pub fn new(db_name: String) -> Self {
@@ -23,12 +27,19 @@ impl TwitterNoteDAO {
             panic!("Couldn't get a DB connection")
         }
     }
-    pub fn add(&self, note: TwitterNote) {
-        let mut statement = self.connection.prepare(INSERT_NOTE_SQL).unwrap();
-        let query = statement.execute(params![&note.user_id, &note.twitter_account_id, &note.text]);
+
+    pub fn exists(&self, user_id: u32, twitter_account_id: u32) -> Option<bool> {
+        let mut statement = self.connection.prepare(EXISTS_NOTE_SQL).unwrap();
+        let query = statement.query(params![&user_id, &twitter_account_id]);
         match query {
-            Ok(updated) => println!("Success"),
-            Err(err) => println!("Fail"),
+            Ok(mut rows) => match rows.next().unwrap() {
+                Some(row) => Some(row.get_unwrap(0)),
+                _ => None,
+            },
+            Err(_err) => {
+                println!("Failure to execute query.");
+                None
+            }
         }
     }
 
@@ -51,11 +62,30 @@ impl TwitterNoteDAO {
         }
     }
 
-    pub fn update(&self, note: TwitterNote) {
-        // do the thing
+    pub fn add(&self, note: TwitterNote) {
+        let mut statement = self.connection.prepare(INSERT_NOTE_SQL).unwrap();
+        let query = statement.execute(params![&note.user_id, &note.twitter_account_id, &note.text]);
+        match query {
+            Ok(updated) => println!("Success"),
+            Err(err) => println!("Fail"),
+        }
     }
 
-    pub fn exists(&self, note: TwitterNote) {
-        // do the thing
+    pub fn update(&self, note: TwitterNote) {
+        let mut statement = self.connection.prepare(UPDATE_NOTE_SQL).unwrap();
+        let query = statement.execute(params![&note.user_id, &note.twitter_account_id, &note.text]);
+        match query {
+            Ok(updated) => println!("Success"),
+            Err(err) => println!("Fail"),
+        }
+    }
+
+    pub fn delete(&self, user_id: u32, twitter_account_id: u32) {
+        let mut statement = self.connection.prepare(DELETE_NOTE_SQL).unwrap();
+        let query = statement.execute(params![&user_id, &twitter_account_id]);
+        match query {
+            Ok(updated) => println!("Success"),
+            Err(err) => println!("Fail"),
+        }
     }
 }
